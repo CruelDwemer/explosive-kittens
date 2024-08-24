ea020e4d31908bb16505b9a16efe9df5aa95cca4
Ilya Vorozhtsov <ilyavorozhtsov39@gmail.com>
2024-08-24 18:49:42 +0300

    fix

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -5,21 +5,17 @@ import Login from './pages/login/login'
 import Register from './pages/register/register'
 
 function App() {
-  // useEffect(() => {
-  //   const fetchServerData = async () => {
-  //     const url = `http://localhost:${__SERVER_PORT__}`
-  //     const response = await fetch(url)
-  //     const data = await response.json()
-  //     console.log(data)
-  //   }
+  useEffect(() => {
+    const fetchServerData = async () => {
+      const url = `http://localhost:${__SERVER_PORT__}`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
 
-  //   fetchServerData()
-  // }, [])
-  return (
-    <div className="App">
-      <Register />
-    </div>
-  )
+    fetchServerData()
+  }, [])
+  return <div className="App">Вот тут будет жить ваше приложение :)</div>
 }
 
 export default App
