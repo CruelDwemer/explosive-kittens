074614c6d7540d078ee104edd284c80eb641d93e
Ilya Vorozhtsov <ilyavorozhtsov39@gmail.com>
2024-08-26 15:51:02 +0300

    routes for login and register added

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,34 +1,49 @@
 import { useEffect } from 'react'
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
+import Login from './pages/login/login'
+import Register from './pages/register/register'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: (
       <div>
-        <h1>Hello World</h1>
-        <div className="App">Вот тут будет жить ваше приложение :)</div>
+        <Link to="/login">Login</Link>
+        <br />
+        <Link to="/register">Register</Link>
       </div>
     ),
   },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
 ])
 
-import Login from './pages/login/login'
-import Register from './pages/register/register'
-
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+  // useEffect(() => {
+  //   const fetchServerData = async () => {
+  //     const url = `http://localhost:${__SERVER_PORT__}`
+  //     const response = await fetch(url)
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
 
-    fetchServerData()
-  }, [])
-  return <RouterProvider router={router} />
+  //   fetchServerData()
+  // }, [])
+  return (
+    <div>
+      <div className="App" style={{ display: 'none', position: 'absolute' }}>
+        Вот тут будет жить ваше приложение :)
+      </div>
+      <RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App
