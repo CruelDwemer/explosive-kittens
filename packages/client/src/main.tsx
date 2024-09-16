1e173a11e475c02e9324d1aeb971a415d8c5b22c
CruelDwemer <cypher3204@gmail.com>
2024-09-16 22:06:28 +0700

    Service worker (#22)
    * ыsw main
    
    * work with cache
    
    * convert to ts

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,6 +4,23 @@ import App from './app/App'
 import './index.css'
 import { withErrorBoundary } from './features'
 
+if ('serviceWorker' in navigator) {
+  try {
+    const registration = await navigator.serviceWorker.register('./sw.ts', {
+      scope: './',
+    })
+    if (registration.installing) {
+      console.log('Service worker installing')
+    } else if (registration.waiting) {
+      console.log('Service worker installed')
+    } else if (registration.active) {
+      console.log('Service worker active')
+    }
+  } catch (error) {
+    console.error(`Registration failed with ${error}`)
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // withErrorBoundary((<App />) as unknown as ComponentType)
   <App />
---
+++ packages/client/sw.ts
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'cache_v1'
+const staticUrl = 'https://ya-praktikum.tech/api/v2/resources/'
+
+;(self as ServiceWorkerGlobalScope).addEventListener('install', event => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME)
+      await cache.addAll(['/public/crocodile.png', '/public/crocodile-win.png'])
+      await self.skipWaiting()
+    })()
+  )
+})
+
+;(self as ServiceWorkerGlobalScope).addEventListener(
+  'activate',
+  async event => {
+    event.waitUntil(self.clients.claim())
+    const cacheNames = await caches.keys()
+    await Promise.all(
+      cacheNames.map(async cacheName => {
+        // Удаляем кэши, которые не относятся к текущей версии
+        if (cacheName !== CACHE_NAME) {
+          await caches.delete(cacheName)
+        }
+      })
+    )
+  }
+)
+
+;(self as ServiceWorkerGlobalScope).addEventListener('fetch', event => {
+  event.respondWith(
+    (async () => {
+      try {
+        const response = await fetch(event.request)
+        // сохраняем только статику
+        if (event.request.url.includes(staticUrl)) {
+          const cache = await caches.open(CACHE_NAME)
+          await cache.put(event.request, response.clone())
+        }
+        return response
+      } catch (error) {
+        const cachedResponse = await caches.match(event.request)
+        if (cachedResponse) {
+          return cachedResponse
+        }
+        return new Response('Connection error', {
+          status: 408,
+          headers: { 'Content-Type': 'text/plain' },
+        })
+      }
+    })()
+  )
+})
