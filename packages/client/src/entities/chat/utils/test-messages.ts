890cb00e41f2f88e49690e0ffd135a19340bb9cc
Ilya Vorozhtsov <ilyavorozhtsov39@gmail.com>
2024-10-20 15:31:40 +0300

    results are saved to leaderboard on yandex

--- packages/client/src/entities/chat/utils/test-messages.ts
+++ packages/client/src/entities/chat/utils/test-messages.ts
@@ -24,6 +24,8 @@ export const testingNewMessages = (
       date: new Date().toISOString(),
       userId: 66,
       userName: 'Вера',
+      userLogin: 'vera281',
+      userAvatar: 'https://ya-praktikum.tech/api/v2/resources/cfa6ee69-52e7-44b1-9414-b0fe96bc458c/b072c577-9cf6-48a0-96c8-e59551fb081c_Cat.jpg',
       content: getMockWord(),
     })
   }, 5000)
@@ -34,6 +36,8 @@ export const testingNewMessages = (
       date: new Date().toISOString(),
       userId: 1,
       userName: 'Вася',
+      userLogin: 'vasya281',
+      userAvatar: 'https://ya-praktikum.tech/api/v2/resources/c5803e2f-c1c2-4af3-8952-78bf5a63de3f/e54ee264-ee30-4d2e-8dc5-01b73f208a70_images.jpg',
       content: getMockWord(),
     })
   }, 3000)
--- packages/client/src/shared/hooks/useLobby.tsx
+++ packages/client/src/shared/hooks/useLobby.tsx
@@ -3,12 +3,14 @@ import { LobbyView } from '../../entities/lobby/models'
 import { useNavigate } from 'react-router'
 import { createSearchParams } from 'react-router-dom'
 import { ROUTER_PATH } from '../models'
-// import { randomWord } from '../../entities/lobby/api'
+import { saveToLeaderboard, getLeaderboard } from '../../entities/leader-board/api'
 
 type SendImageFunc = (guessingImage: string) => void
 type StartNewRoundFunc = (
   guessedUserId: number,
-  guessedUserName: string
+  guessedUserName: string,
+  guessedUserLogin: string,
+  guessedUserAvatar: string
 ) => void
 type UseLobbyHook = (data: { lobbyId: number; currentUserId: number }) => {
   id: number
@@ -19,6 +21,8 @@ type UseLobbyHook = (data: { lobbyId: number; currentUserId: number }) => {
   startNewRound: StartNewRoundFunc
   close: VoidFunction
 }
+type Entry = { name: string, login: string, score: number, avatar: string }
+type Rating = Record<number, Entry>
 type UseLobbyHookState = {
   id: number
   view: LobbyView
@@ -27,7 +31,7 @@ type UseLobbyHookState = {
   image: string | null
 
   // TODO: Сохранять локально рейтинг, чтоб передать его в лидерборд
-  rating: Record<number, { name: string; score: number }>
+  rating: Rating
 }
 
 const useLobby: UseLobbyHook = ({ lobbyId, currentUserId }) => {
@@ -73,7 +77,9 @@ const useLobby: UseLobbyHook = ({ lobbyId, currentUserId }) => {
 
   const changeGuessingToWaiting: StartNewRoundFunc = (
     guessedUserId,
-    guessedUserName
+    guessedUserName,
+    guessedUserLogin,
+    guessedUserAvatar
   ) => {
     // TODO: API запрос отправки id уагадавшего в общий чат для того, что бы начислить ему балл
     const { rating } = lobbyData
@@ -82,7 +88,7 @@ const useLobby: UseLobbyHook = ({ lobbyId, currentUserId }) => {
     if (userRating) {
       copy[guessedUserId].score += 1
     } else {
-      copy[guessedUserId] = { name: guessedUserName, score: 1 }
+      copy[guessedUserId] = { name: guessedUserName, login: guessedUserLogin, score: 1, avatar: guessedUserAvatar }
     }
     console.log(guessedUserId)
 
@@ -108,7 +114,7 @@ const useLobby: UseLobbyHook = ({ lobbyId, currentUserId }) => {
   const deleteLobby = () => {
     const { rating } = lobbyData
 
-    saveToLeaderboard(rating)
+    saveResultsToLeaderboard(rating)
 
     // TODO: Запрос на запись результата в лидерборд
     // TODO: Запрос на удаление всех пользователей из лобби
@@ -126,8 +132,51 @@ const useLobby: UseLobbyHook = ({ lobbyId, currentUserId }) => {
     })
   }
 
-  const saveToLeaderboard = rating => {
-    console.log('RESULTS: ', rating)
+  const saveResultsToLeaderboard = async (rating: Rating) => {
+    const response = await getLeaderboard(JSON.stringify({
+      ratingFieldName: "catsRating",
+      cursor: 0,
+      limit: 10
+    }));
+    const result = await response.json();
+    const { data } = result[0];
+    const { ratings } = result[0].data;
+
+    // console.log("WINNER OF THE GAME: ", rating)
+    // console.log("RECIEVED DATA: ", data, "RATINGS: ", ratings)
+
+    Object.values(rating).forEach((winner) => {
+      const match = ratings.find((entry: Entry) => winner.login === entry.login)
+      // console.log("FOUND MATCH ", match)
+
+      if (!match) {
+        ratings.push(winner)
+      } else {
+        const index = ratings.findIndex((entry: Entry) => entry.login === match.login)
+        // console.log("INDEX: ", index, "RATINGS: ", ratings, 'RAT IND: ', ratings[index])
+        ratings[index].score += winner.score
+      }
+    })
+    data.ratings.sort((a: Entry, b: Entry) => b.score - a.score)
+    ++data.catsRating;
+
+    const requestData = {
+      data,
+      ratingFieldName: "catsRating"
+    }
+
+    await saveToLeaderboard(JSON.stringify(requestData));
+    finalCheck()
+  }
+
+  async function finalCheck() {
+    const response = await getLeaderboard(JSON.stringify({
+      ratingFieldName: "catsRating",
+      cursor: 0,
+      limit: 10
+    }));
+    const result = await response.json();
+    console.log("LEADERBOARD RESULT: ", result)
   }
 
   return {
