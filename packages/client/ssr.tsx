4b8f1d9b109d9c4f04b89f0e23d55d14837854d1
dergunovmxm <Derga13@yandex.ru>
2024-10-21 19:25:47 +0400

    ssr

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,6 +1,24 @@
+import React from 'react'
 import App from './src/app/App'
 import { renderToString } from 'react-dom/server'
+import type { Request, Response } from 'express'
+import { Provider } from 'react-redux'
+import { createSsrStore } from './src/shared/lib'
+import {
+  createStaticRouter,
+  createStaticHandler,
+} from 'react-router-dom/server'
 
-export function render() {
-  return renderToString(<App />)
+export function render(req: Request, res: Response) {
+  const store = createSsrStore()
+  const initialState = store.getState()
+
+  return {
+    html: renderToString(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    ),
+    preloadedState: initialState,
+  }
 }
