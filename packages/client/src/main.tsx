b2e0eef88603008b84d2e2df91908dbae1fe3aca
BMike <cypher3204@gmail.com>
2024-10-23 02:16:02 +0700

    fixes for starting with docker-compose

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,22 +4,22 @@ import './index.css'
 import { store } from './shared/lib'
 import { Provider } from 'react-redux'
 
-if ('serviceWorker' in navigator) {
-  try {
-    const registration = await navigator.serviceWorker.register('./sw.ts', {
-      scope: './',
-    })
-    if (registration.installing) {
-      console.log('Service worker installing')
-    } else if (registration.waiting) {
-      console.log('Service worker installed')
-    } else if (registration.active) {
-      console.log('Service worker active')
-    }
-  } catch (error) {
-    console.error(`Registration failed with ${error}`)
-  }
-}
+// if ('serviceWorker' in navigator) {
+//   try {
+//     const registration = await navigator.serviceWorker.register('./sw.ts', {
+//       scope: './',
+//     })
+//     if (registration.installing) {
+//       console.log('Service worker installing')
+//     } else if (registration.waiting) {
+//       console.log('Service worker installed')
+//     } else if (registration.active) {
+//       console.log('Service worker active')
+//     }
+//   } catch (error) {
+//     console.error(`Registration failed with ${error}`)
+//   }
+// }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // withErrorBoundary((<App />) as unknown as ComponentType)
