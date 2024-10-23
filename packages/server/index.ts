2ef055c547dbada26b1d0d40c2ed698b48d85710
Анастасия Абазовская <ryabayeah@gmail.com>
2024-10-23 22:23:10 +0700

    connect router

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -5,6 +5,8 @@ dotenv.config()
 import express from 'express'
 import { createClientAndConnect } from './db'
 
+import { apiRouter } from './api'
+
 const app = express()
 app.use(cors())
 const port = Number(process.env.SERVER_PORT) || 3001
@@ -14,6 +16,7 @@ createClientAndConnect()
 app.get('/', (_, res) => {
   res.json('👋 Howdy from the server :)')
 })
+app.use('/api/v1', apiRouter)
 
 app.listen(port, () => {
   console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
