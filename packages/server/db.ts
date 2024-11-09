a0ba1e8f86ef693ab7494ef2f463fcb9a70e6cb8
Ilya Vorozhtsov <ilyavorozhtsov39@gmail.com>
2024-11-09 12:49:14 +0300

    fix

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -3,13 +3,13 @@ import { TopicModel } from './api/topic/topic.model'
 import { UserModel } from './api/user/user.model'
 import { CommentModel } from './api/comment/comment.model'
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST } =
   process.env
 
 export const initPostgress = async (): Promise<Sequelize | null> => {
 
   const sequelizeOptions: SequelizeOptions = {
-    host: 'localhost',
+    host: POSTGRES_HOST,
     port: Number(POSTGRES_PORT),
     username: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
