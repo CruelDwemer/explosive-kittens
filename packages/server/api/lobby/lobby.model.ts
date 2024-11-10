4f2336f9341294e33550f75a820b40990750fb6f
Анастасия Абазовская <ryabayeah@gmail.com>
2024-11-10 20:01:12 +0700

    add model

---
+++ packages/server/api/lobby/lobby.model.ts
@@ -0,0 +1,44 @@
+import {
+  Model,
+  Column,
+  DataType,
+  AllowNull,
+  Table,
+  PrimaryKey,
+  AutoIncrement,
+  ForeignKey,
+} from 'sequelize-typescript'
+import { UserModel } from '../user/user.model'
+
+@Table({ tableName: 'lobby' })
+export class LobbyModel extends Model<LobbyModel> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  lobbyId: number
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  name: string
+
+  @ForeignKey(() => UserModel)
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  ownerId: UserModel
+
+  @ForeignKey(() => UserModel)
+  @AllowNull(true)
+  @Column(DataType.INTEGER)
+  leadId: UserModel
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  state: 'guessing' | 'drawing' | 'not_started'
+
+  @Column(DataType.STRING)
+  @AllowNull(true)
+  currentWord: string
+
+  @Column(DataType.JSON)
+  history: { userId: number; word: string; date: string }[]
+}
